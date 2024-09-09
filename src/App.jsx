761d5d081931921467b58344c81d2f49f4cb3eb8
K0sarr <Kosar_Abobakri@yahoo.com>
2024-09-09 12:15:56 +0200

    Added working movieApi and fetching code
    I have added the movieAPI for the webapp. Also with working fetch for searching movies.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
+const API_URL = 'http://omdbapi.com?apikey=3e1b65eb';
 const App = () => {
+
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.Search);
+  }
+
+  useEffect(() => {
+    searchMovies('Shrek');
+  }, []);
+
+
   return (
     <div>
       <h1>HEY app</h1>
