3e313d3491d3de71b5e743a6298c4475fc0f1909
K0sarr <Kosar_Abobakri@yahoo.com>
2024-09-09 12:56:39 +0200

    Update application with useEffect API

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,21 @@ const API_URL = 'http://omdbapi.com?apikey=3e1b65eb';
 }
 
 const App = () => {
+
+  const [movies, setMovies] = useState('');
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.Search)
+  }
+
+  useEffect(() => {
+    searchMovies('Shrek');
+  }, [])
+
+
   return (
     <div className='app'>
       <h1>MoviePedia</h1>
